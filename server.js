579de78fdf9dbc4e5f58571c6ec5d38b722ae738
abdul aziz <ajitagustiani10@gmail.com>
2019-10-29 10:38:24 +0700

    membuat request post

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express')
 const app = express()
 const port= 3000
 
+//membuat request post
+app.post('/hello', function(req,res){
+    const response={
+        statusCode:200,
+        error:"",
+        message: "hello json",
 
+    }
+    res.json(response);
+})
+
+//commit lagi dengan nama membuat request post
 app.get('/',(req,res)=> res.send("hello dunia"))
 
 app.listen(port, ()=> console.log('example app listening on port ${port}!'))
