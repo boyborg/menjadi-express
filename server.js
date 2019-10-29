0580cce4896840cc0ef5463388310531bc06a7e8
abdul aziz <ajitagustiani10@gmail.com>
2019-10-29 11:31:09 +0700

    config body-parser2

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const app = express()
 const port= 3000
 //config body-parser
 const bodyParser= require("body-parser")
+app.use(bodyParser.urlencoded({extended:true})); //menangkap tipe request dalam bentu from encoded
+app.use(bodyParser.json()); //mengkap url dalam bentuk json
+//commit lagi dengan nama config body-parser
 
 //membuat request post
+//nama request first name,lastname
 app.post('/hello', function(req,res){
     const response={
         statusCode:200,
         error:"",
-        message: "hello json",
+        message: "hello jsonf",
+        conntent:req.body
 
     }
     res.json(response);
@@ -18,7 +23,6 @@ app.post('/hello', function(req,res){
 //commit lagi dengan nama membuat request post
 app.get('/',(req,res)=> res.send("hello dunia"))
 //config body-parser
-app.use(bodyParser.urlencoded({extended:true})); //menangkap tipe request dalam bentu from encoded
-app.use(bodyParser.json()); //mengkap url dalam bentuk json
+
 
 app.listen(port, ()=> console.log('example app listening on port ${port}!'))
